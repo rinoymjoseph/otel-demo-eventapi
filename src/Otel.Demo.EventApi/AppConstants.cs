3a6e1584fff8469f9cf01d0daeb2c5bc98676a9a
Rinoy Joseph <rinoymjoseph@gmail.com>
2023-06-17 14:36:01 +0530

    updated

--- src/Otel.Demo.EventApi/AppConstants.cs
+++ src/Otel.Demo.EventApi/AppConstants.cs
@@ -7,7 +7,7 @@
         public const string URL_OTEL_EXPORTER = "otel_exporter_url";
         public const string URL_DATA_API = "data_api_url";
 
-        public const string REQUEST_GET_EVENTS = "/assetdb/GetEvents";
+        public const string REQUEST_GET_EVENTS = "/data/GetEvents";
 
         public const string COUNTER_EVENT_GET_EVENTS_OF_ASSET = "event_api_get_events_of_asset_requests";
     }
